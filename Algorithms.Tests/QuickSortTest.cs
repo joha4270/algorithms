575ea62b46f4c137a7d70b385f7fd3b5970aee60
Johannes Elgaard <joha4270@gmail.com>
2017-01-08 16:09:56 +0100

    Added quicksort

---
+++ Algorithms.Tests/QuickSortTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture()]
+    public class QuickSortTest
+    {
+        [Test]
+        [TestCase(8)]
+        [TestCase(13)]
+        [TestCase(1024)]
+        public void TestReverse(int size)
+        {
+            int[] testArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                testArray[i] = size - i;
+            }
+            Quicksort.Sort(testArray);
+
+            CollectionAssert.IsOrdered(testArray);
+        }
+
+        [Test]
+        [TestCase(8)]
+        [TestCase(13)]
+        [TestCase(1024)]
+        public void TestAllreadySorted(int size)
+        {
+            int[] testArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                testArray[i] = i;
+            }
+            Quicksort.Sort(testArray);
+
+            CollectionAssert.IsOrdered(testArray);
+        }
+
+        [Test]
+        [TestCase(8)]
+        [TestCase(13)]
+        [TestCase(1024)]
+        public void TestRandom(int size)
+        {
+            int[] testArray = new int[size];
+            Random random = new Random(1);
+            for (int i = 0; i < size; i++)
+            {
+                testArray[i] = i;
+            }
+
+            for (int i = 0; i < size-1; i++)
+            {
+                int randomIndex = random.Next(i+1, size );
+
+                int temp = testArray[i];
+                testArray[i] = testArray[randomIndex];
+                testArray[randomIndex] = temp;
+            }
+            Quicksort.Sort(testArray);
+
+            CollectionAssert.IsOrdered(testArray);
+        }
+
+        [Test]
+        [TestCase(8)]
+        [TestCase(13)]
+        [TestCase(1024)]
+        public void TestMostlySame(int size)
+        {
+            int[] testArray = new int[size];
+            Random random = new Random(1);
+            for (int i = 0; i < size; i++)
+            {
+                testArray[i] = random.Next(4);
+            }
+            Quicksort.Sort(testArray);
+
+            CollectionAssert.IsOrdered(testArray);
+        }
+
+        [Test]
+        public void TestOddOneOut()
+        {
+            int[] testArray = new[] {1, 1, 1, 1, 5, 1, 1,};
+            Quicksort.Sort(testArray);
+
+            CollectionAssert.IsOrdered(testArray);
+        }
+    }
+}
---
+++ Algorithms/Sorting/Quicksort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Quicksort
+    {
+        public static void Sort<T>(IList<T> array) where T : IComparable<T>
+        {
+            Sort(array, (T x, T y) => x.CompareTo(y));
+        }
+
+        public static void Sort<T>(IList<T> array, Comparison<T> compare)
+        {
+            QuickSort(array, compare, 0, array.Count);
+        }
+
+        private static void QuickSort<T>(IList<T> array, Comparison<T> compare, int beginning, int afterLast)
+        {
+            if (beginning < afterLast-1)
+            {
+                int pivot = Partition(array, compare, beginning, afterLast);
+                QuickSort(array, compare, beginning, pivot);
+                QuickSort(array, compare, pivot+1, afterLast);
+
+            }
+        }
+
+        private static int Partition<T>(IList<T> array, Comparison<T> compare, int beginning, int afterLast)
+        {
+            T temp;
+            T middle = array[afterLast - 1];
+            int pivot = beginning;
+
+            for (int i = beginning; i < afterLast; i++)
+            {
+                if (compare(array[i],middle) < 0)
+                {
+                    Swarp(array, pivot, i);
+
+                    pivot++;
+                }
+            }
+
+            Swarp(array, pivot, afterLast-1);
+
+            return pivot;
+        }
+
+        private static void Swarp<T>(IList<T> array, int pivot, int i)
+        {
+            T temp;
+            temp = array[pivot];
+            array[pivot] = array[i];
+            array[i] = temp;
+        }
+    }
+}
