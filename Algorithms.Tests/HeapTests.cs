a01e7cef5592d1757331a5c4757e964695295bcf
Johannes Elgaard <joha4270@gmail.com>
2017-01-08 13:17:40 +0100

    Initial commit

---
+++ Algorithms.Tests/HeapTests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tests
+{
+    [TestFixture()]
+    public class HeapTests
+    {
+        [Test()]
+        public void HeapifyTest1()
+        {
+            int[] arr = new[] {1, 2, 3, 4, 5, 6};
+            Heap.MaxHeapify(arr);
+            TestContext.WriteLine(string.Join(", ", arr));
+            Assert.That(Heap.IsMaxHeap(arr));
+        }
+
+        [Test()]
+        public void HeapifyTest2()
+        {
+            int[] arr = new[] { 1,2,2,4,4,4,4,8,8,8 };
+            Heap.MaxHeapify(arr);
+            TestContext.WriteLine(string.Join(", ", arr));
+            Assert.That(Heap.IsMaxHeap(arr));
+        }
+
+        [Test()]
+        public void HeapifyTest3()
+        {
+            int[] arr = new[] { 5,2,5,2,5,2,1 };
+            Heap.MaxHeapify(arr);
+            TestContext.WriteLine(string.Join(", ", arr));
+            Assert.That(Heap.IsMaxHeap(arr));
+        }
+
+        [Test()]
+        public void HeapifyTest4()
+        {
+            int[] arr = new[] {6,5,5,5,5,5 };
+            Heap.MaxHeapify(arr);
+            TestContext.WriteLine(string.Join(", ", arr));
+            Assert.That(Heap.IsMaxHeap(arr));
+        }
+
+        [Test()]
+        public void HeapifyTest5()
+        {
+            int[] arr = new[] {6,6,6,6, 6 };
+            Heap.MaxHeapify(arr);
+            TestContext.WriteLine(string.Join(", ", arr));
+
+            Assert.That(Heap.IsMaxHeap(arr));
+        }
+
+        [Test()]
+        public void HeapifyTest6()
+        {
+            int[] arr = new[] { 5,5,5,5,5,6 };
+            Heap.MaxHeapify(arr);
+            TestContext.WriteLine(string.Join(", ", arr));
+            Assert.That(Heap.IsMaxHeap(arr));
+        }
+
+        //[Test()]
+        //[TestCase(new[] {1, 2}, false)]
+        //[TestCase(new[] {1, 2}, false)]
+        //[TestCase(new[] {1, 2}, false)]
+        //[TestCase(new[] {1, 2}, false)]
+        //[TestCase(new[] {1, 2}, false)]
+        //public void IsHeapTest(int[] values, bool result)
+        //{
+        //    Assert.Pass();
+        //    Assert.AreEqual(result, Heap.IsMaxHeap(values));
+        //}
+
+
+        [Test()]
+        public void IsHeapTest1()
+        {
+            Assert.AreEqual(false, Heap.IsMaxHeap(new[] {1, 2}));
+        }
+
+        [Test()]
+        public void IsHeapTest2()
+        {
+            Assert.AreEqual(false, Heap.IsMaxHeap(new[] { 1, 2, 3 }));
+        }
+
+        [Test()]
+        public void IsHeapTest3()
+        {
+            Assert.AreEqual(true, Heap.IsMaxHeap(new[] { 3, 1, 1 }));
+        }
+
+        [Test()]
+        public void IsHeapTest4()
+        {
+            Assert.AreEqual(true, Heap.IsMaxHeap(new[] { 7, 6, 4, 5, 3, 2, 4 }));
+        }
+
+        [Test()]
+        public void IsHeapTest5()
+        {
+            Assert.AreEqual(false, Heap.IsMaxHeap(new[] { 7, 6, 4,4,  3, 5, 2, 4 }));
+        }
+    }
+}
---
+++ Algorithms/Heap.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Heap
+    {
+        public static void MaxHeapify<T>(IList<T> list) where T : IComparable<T>
+        {
+            for (int i = (list.Count -1) / 2; i >= 0; i--)
+            {
+                MaxHeapify(list, i);
+            }
+        }
+
+        private static void MaxHeapify<T>(IList<T> list, int pos) where T: IComparable<T>
+        {
+            int l = Left(pos);
+            int r = Right(pos);
+            int largest = pos;
+
+            if (l < list.Count && list[l].CompareTo(list[pos]) > 0)
+            {
+                largest = l;
+            }
+            if (r < list.Count && list[r].CompareTo(list[largest]) > 0)
+            {
+                largest = r;
+            }
+
+            if (largest != pos)
+            {
+                Swarp(list, largest, pos);
+                MaxHeapify(list, largest);
+            }
+
+        }
+
+        public static bool IsMaxHeap<T>(IList<T> list) where T : IComparable<T>
+        {
+            return IsMaxHeap<T>(list, 0);
+        }
+
+        private static bool IsMaxHeap<T>(IList<T> list, int n) where T : IComparable<T>
+        {
+            //if (n >= list.Count) return true;
+            int l = Left(n), r = Right(n);
+            bool left = l >= list.Count || list[n].CompareTo(list[l]) >= 0;
+
+            bool right = r >= list.Count || list[n].CompareTo(list[r]) >= 0;
+
+            if (left && right)
+            {
+                bool left2 = true, right2 = true;
+                if (l < list.Count)
+                {
+                    left2 = IsMaxHeap(list, l);
+                }
+
+                if (r < list.Count)
+                {
+                    right2 = IsMaxHeap(list, r);
+                }
+                return (right2 && left2);
+            }
+            return false;
+        }
+
+        private static int Left(int n) => 2*n + 1;
+
+        private static int Right(int n) => 2*n + 2;
+
+        private static int Parrent(int n) => (n-1)/2;
+
+        private static void Swarp<T>(IList<T> list, int a, int b)
+        {
+            T temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+    }
+}
